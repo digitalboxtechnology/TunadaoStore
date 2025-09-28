5895bd9f558fd4efee9c55be3bc320e4c6f43d45
WELLINGTON  NETO <116044216+net0well@users.noreply.github.com>
2025-09-27 23:43:19 -0300

    Recuperando alterações da main

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce37f451ba028958bcf36222feb2dc511ca343a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f634ce5d952091d538513574eab7c58a4775bbd7")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
