acc812a11a3ec79bd5c143978d7360fc19b1d6e3
WELLINGTON  NETO <116044216+net0well@users.noreply.github.com>
2025-10-01 22:01:52 -0300

    adding sorting to the specification

--- Core/Specifications/BaseSpecification.cs
+++ Core/Specifications/BaseSpecification.cs
@@ -17,4 +17,19 @@ public class BaseSpecification<T> : ISpecification<T>
     }
 
     public Expression<Func<T, bool>>? Criteria => criteria;
+
+    public Expression<Func<T, object>>? OrderBy { get; private set; }
+
+    public Expression<Func<T, object>>? OrderByDescending { get; private set; }
+
+    protected void AddOrderBy(Expression<Func<T, object>> orderByExpression)
+    {
+        OrderBy = orderByExpression;
+    }
+
+    protected void AddOrderByDescending(Expression<Func<T, object>> orderByDescExpression)
+    {
+        OrderByDescending = orderByDescExpression;
+    }
+ 
 }
